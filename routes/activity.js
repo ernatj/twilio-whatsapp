688462da0564fec5765e38bdcc53eab25e4f4cca
heraldoyp <heraldo@blendmedia.co.id>
2021-12-05 09:48:10 +0700

    #q

--- routes/activity.js
+++ routes/activity.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 const qs = require('qs');
 var http = require('https');
 var request = require('request');
+const { response } = require('express');
 
 exports.logExecuteData = [];
 
@@ -94,42 +95,73 @@ exports.execute = function (req, res) {
     const mcToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IjQiLCJ2ZXIiOiIxIiwidHlwIjoiSldUIn0.eyJhY2Nlc3NfdG9rZW4iOiJZNVYxWXJ3NnYzbWtBaTh4eTc0WTNhWUIiLCJjbGllbnRfaWQiOiJzcm11NDl4NXExdzJ2amV5OXN1ZWY2NHciLCJlaWQiOjExMDAwNjQ3NCwic3RhY2tfa2V5IjoiUzExIiwicGxhdGZvcm1fdmVyc2lvbiI6MiwiY2xpZW50X3R5cGUiOiJTZXJ2ZXJUb1NlcnZlciJ9.gnmpwr17TIfIcCJVFuu1gwmrvWZipe_jgkfj_8us3tM.umIJrT07Ve6EyZyixeTdI1hPeX13-JlvguU8Lc5QUYdvaRusktM6tZSSpGRlRdORNQxu1PfercHJmSpX_icgNHgVM1zIMZGEFcPo5VQ0CT0Sq_jC4nyPkbta4vip4rZtcnDsUBF3yAR76b809chtlZDtRVMvXlH7haKgbk6Z2jIMe3PjR7k"
     const dataextensionId = "F8BCBDCC-6526-49BD-BCBE-CC4A906FE0D2"
 
-    axios.post("https://api.twilio.com/2010-04-01/Accounts/"+accountId+"/Messages.json", qs.stringify({
-        'Body': req.body.inArguments[0].Message,
-        'From': 'whatsapp:+'+ req.body.inArguments[0].Sender,
-        'To': 'whatsapp:+'+ req.body.inArguments[0].Mobile,
-    }), {
-      auth: {
-        username: accountId,
-        password: authToken
-      }
-    })
-    .then(response => {
-        //Send Status to Data Extension for Updates
-        console.log("Response Data"+util.inspect(response.data));
-
-        var config = {
-            'headers': {
-                'Authorization': 'Bearer '+mcToken
+    var response = {
+        "data":
+            {
+                "items": [{
+                    'sid': 'SM90a4ab1874da40879dee0b5f16c16dc',
+                    'date_created': 'Sat, 04 Dec 2021 07:06:29 +0000',
+                    'date_updated': 'Sat, 04 Dec 2021 07:06:29 +0000',
+                    'date_sent': null,
+                    'account_sid': 'ACb494ea5723f3f2f591bbc092b094d41d',
+                    'to': 'whatsapp:+6285719752942',
+                    'from': 'whatsapp:+14155238886',
+                    'messaging_service_sid': null,
+                    'body': 'Ini test message dari custom journey builider version 4',
+                    'status': 'queued',
+                    'num_segments': '1',
+                    'num_media': '0',
+                    'direction': 'outbound-api',
+                    'api_version': '2010-04-01',
+                    'price': null,
+                    'price_unit': null,
+                    'error_code': null,
+                    'error_message': null,
+                    'uri': '/2010-04-01/Accounts/ACb494ea5723f3f2f591bbc092b094d41d/Messages/SM90a4ab1874da40879dee0b5f16c16dc2.json',
+                    'subresource_uris': "{ media: '/2010-04-01/Accounts/ACb494ea5723f3f2f591bbc092b094d41d/Messages/SM90a4ab1874da40879dee0b5f16c16dc2/Media.json' } }"
+                }]
             }
-        }
 
-        var data = {
-            'items': [response.data]
+    }
+
+    var config = {
+        'headers': {
+            'Authorization': 'Bearer '+mcToken
         }
-        
-        axios.post(`https://mcx3dk6gqx05byn626r3yqc9-hl0.rest.marketingcloudapis.com/data/v1/async/dataextensions/key:${dataextensionId}/rows`, data, config)
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+    }
+
+    var data = {
+        'items': [response.data]
+    }
+    
+    axios.post(`https://mcx3dk6gqx05byn626r3yqc9-hl0.rest.marketingcloudapis.com/data/v1/async/dataextensions/key:${dataextensionId}/rows`, data, config)
+    .then(response => {
+        console.log(response);
     })
     .catch(error => {
-      console.log('Auth '+authToken+' Account SID '+accountId);
-      console.log(error);
-    }) 
+        console.log(error);
+    })
+
+    // axios.post("https://api.twilio.com/2010-04-01/Accounts/"+accountId+"/Messages.json", qs.stringify({
+    //     'Body': req.body.inArguments[0].Message,
+    //     'From': 'whatsapp:+'+ req.body.inArguments[0].Sender,
+    //     'To': 'whatsapp:+'+ req.body.inArguments[0].Mobile,
+    // }), {
+    //   auth: {
+    //     username: accountId,
+    //     password: authToken
+    //   }
+    // })
+    // .then(response => {
+    //     //Send Status to Data Extension for Updates
+    //     console.log("Response Data"+util.inspect(response.data));
+
+    //     // Axios to Data Extension
+    // })
+    // .catch(error => {
+    //   console.log('Auth '+authToken+' Account SID '+accountId);
+    //   console.log(error);
+    // }) 
 
     console.log('ISI DATA EXTENSION =>'+req.body.inArguments[0].DataExtension);
     console.log('ISI SENDER =>'+req.body.inArguments[0].Sender);
